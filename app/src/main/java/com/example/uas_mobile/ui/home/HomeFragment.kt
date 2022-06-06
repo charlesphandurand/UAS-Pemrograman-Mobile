2db10fe230ffbea54e4bd09c40443a8b70c71219
charles_phandurand <charles.phandurand@gmail.com>
2022-06-06 11:36:04 +0800

    Menambahkan home dan splash activity

--- app/src/main/java/com/example/uas_mobile/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/uas_mobile/ui/home/HomeFragment.kt
@@ -27,7 +27,7 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.textView
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
